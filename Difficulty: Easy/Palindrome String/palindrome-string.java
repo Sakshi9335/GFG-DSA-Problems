6598ab06b3663840ea7ea5bf38c5f004d74b4dcf
Sakshi Shukla <sakshishu09@gmail.com>
2025-09-28 18:12:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome String/palindrome-string.java
@@ -0,0 +1,20 @@
+
+
+// User function Template for Java
+
+class Solution {
+    boolean isPalindrome(String s) {
+        // code here
+        int i=0;
+        int j= s.length()-1;
+        while (i<=j){
+            if (s.charAt(i)!=s.charAt(j))
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
