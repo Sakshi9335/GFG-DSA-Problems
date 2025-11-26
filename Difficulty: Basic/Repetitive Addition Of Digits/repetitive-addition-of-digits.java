ed19ca84a34d939eee2ece36bdd6aabc3e6adca6
Sakshi Shukla <sakshishu09@gmail.com>
2025-11-26 09:32:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repetitive Addition Of Digits/repetitive-addition-of-digits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleDigit(int n) {
+        // code here
+        if(n==0){
+            return 0;
+        }
+        if(n%9==0){
+            return 9;
+        }
+        else 
+        return n%9;
+        
+    }
+}
