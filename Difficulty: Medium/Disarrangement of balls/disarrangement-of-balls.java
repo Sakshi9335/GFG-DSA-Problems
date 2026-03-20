f9739aed106088de9f62a606f1da427c5a749ffd
Sakshi Shukla <sakshishu09@gmail.com>
2026-03-20 11:41:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Disarrangement of balls/disarrangement-of-balls.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+class Solution {
+    static int countDer(int n) {
+        // Base case
+        if(n<=3) return n-1;
+        return (n-1)*(countDer(n-1)+countDer(n-2));
+    }
+}
