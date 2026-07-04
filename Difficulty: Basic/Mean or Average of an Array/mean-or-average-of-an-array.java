69f7fba43942944de7a0d6d6225aee310c61a3b5
Sakshi Shukla <sakshishu09@gmail.com>
2026-07-04 19:59:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Mean or Average of an Array/mean-or-average-of-an-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int findMean(int[] arr) {
+        // code here
+        int sum=0;
+        int n=arr.length;
+        for(int i=0; i<n;i++){
+            sum=sum+arr[i];
+        }
+        return sum/n;
+    }
+};
